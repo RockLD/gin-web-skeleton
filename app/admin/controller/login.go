b4ba2a8b48757486307a9d01dc828888f33ca0dc
RockLD <lid18149@gmail.com>
2020-08-13 23:28:40 +0800

    loginfield

--- app/admin/controller/login.go
+++ app/admin/controller/login.go
@@ -1,14 +1,16 @@
 package controller
 
 import (
+	"fmt"
 	"gin-web-skeleton/util"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type LoginField struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `form:"username"`
+	Password string `form:"password"`
+	Csrf     string `form:"csrf"`
 }
 
 type RespData struct {
@@ -20,13 +22,16 @@ func Login(c *gin.Context) {
 	if "POST" == c.Request.Method {
 		var loginField LoginField
 		if err := c.Bind(&loginField); err != nil {
+			fmt.Println(err)
 			util.SendResponse(c, err, nil)
-			c.Abort()
+			return
 		}
 		var respData RespData
 		respData.Username = loginField.Username
 		respData.Password = loginField.Password
+		fmt.Println(respData)
 		util.SendResponse(c, nil, respData)
+		return
 	}
 	//token := csrf.GetToken(c)
 	c.HTML(http.StatusOK, "login/login.html", nil)
